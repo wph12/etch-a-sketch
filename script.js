57d44518f6f5711cc5ba45a5f034f3c4be016659
wph12 <peiheweng@gmail.com>
2023-05-07 12:41:23 +0800

    add logic for change grid size

--- script.js
+++ script.js
@@ -1,42 +1,72 @@
-function createGrid() {
-    const sketchpad = document.getElementById("sketchpad")
-    for(let i = 0; i < 8; i++){
-        sketchpad.innerHTML += '<div class = "row" ></div>'
+function createGrid(size) {
+    let sketchpad = document.getElementById("sketchpad")
+    console.log(size);
+    sketchpad.innerHTML = ''
+    for(let i = 0; i < size; i++){
+        sketchpad.innerHTML += '<div class = "row" ></div>';
     }
 
     rowArray = document.getElementsByClassName("row")
     console.log(rowArray)
-    for(k = 0; k < 8; k++){
-        for(let j = 0; j < 8; j++){
-            rowArray[k].innerHTML += '<div class = "square" ></div>'
+    for(k = 0; k < size; k++){
+        for(let j = 0; j < size; j++){
+            rowArray[k].innerHTML += '<div class = "square" ></div>';
         }
+    }
+
+}
+
+
+function getSize(){
+    let text = prompt("Please enter your desired grid size (1-100)", "8");
+    let size = parseInt(text); 
+    if(size){
+        if(size >= 1 && size <= 100){
+            return size;
+        }
+    }
+    else{
+        alert("Please enter a valid integer from range 1-100!")
     }
 }
 
-createGrid();
+createGrid(8);
+createSquares();
 
 
-const squares = document.querySelectorAll("div.square")
-var isMouseDown = false
 
-document.getElementById("sketchpad").addEventListener('mouseleave', () => {
-    isMouseDown = false;
-})
 
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.addEventListener('mousedown', () => {
-            isMouseDown = true;
-        })
 
-        square.addEventListener('mouseup', () => {
-            isMouseDown = false;
-        })
+function createSquares() {
+    const squares = document.querySelectorAll("div.square")
+    var isMouseDown = false
 
-        square.addEventListener('mousemove', () =>{
-            if(isMouseDown){
-                square.style.backgroundColor = "black";
-            }
-        }) 
+    document.getElementById("sketchpad").addEventListener('mouseleave', () => {
+        isMouseDown = false;
     })
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.addEventListener('mousedown', () => {
+                isMouseDown = true;
+                square.style.backgroundColor = "black";
+            })
+    
+            square.addEventListener('mouseup', () => {
+                isMouseDown = false;
+            })
+    
+            square.addEventListener('mousemove', () =>{
+                if(isMouseDown){
+                    square.style.backgroundColor = "black";
+                }
+            }) 
+        })
+    })
+}
+
+
+document.getElementById("changeSize").addEventListener('click', ()=> {
+    createGrid(getSize());
+    createSquares();
 })
